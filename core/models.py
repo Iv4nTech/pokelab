1fd42d4fe40775824a57324008427f13fbbaea99
Iv4nGomez <xigomjim596@ieshnosmachado.org>
2025-12-05 13:51:27 +0100

    Inicilize project and create BD

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=50)
+    colour_hex = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.type}"
+    
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+    
+class Pokemon(models.Model):
+    nickname = models.CharField(max_length=50)
+    level = models.IntegerField(default=1)
+    pokedex_number = models.IntegerField()
+    image_url = models.URLField()
+    attack = models.IntegerField()
+    defense = models.IntegerField()
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+    type = models.ManyToManyField(Type)
+
+    def __str__(self) -> str:
+        return f"{self.nickname}"
